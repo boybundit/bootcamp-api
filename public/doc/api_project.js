a6e3ad8f67cc331a0d12a537146ca7521b4c7aaa
Bundit J <Bundit J>
2016-08-27 22:42:24 +0700

    rerun apidoc

--- public/doc/api_project.js
+++ public/doc/api_project.js
@@ -3,7 +3,7 @@ define({
   "version": "1.0.0",
   "description": "This is a Boot Camp API project.",
   "title": "Boot Camp API",
-  "url": "https://bootcamp-dev.azurewebsites.net/",
+  "url": "https://bootcamp-dev.azurewebsites.net",
   "order": [
     "GetUser",
     "PostUser"
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-08-27T15:35:30.938Z",
+    "time": "2016-08-27T15:42:08.175Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
