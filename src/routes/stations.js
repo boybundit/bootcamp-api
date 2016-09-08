9f1ea9dec8c159c76820ce8ddc0784329a02730a
Kriangkrai Traichaiyaporn <belteus.kk@gmail.com>
2016-09-08 15:49:31 +0700

    get stations by keyword and support 2 languages

--- src/routes/stations.js
+++ src/routes/stations.js
@@ -2,34 +2,75 @@ var express = require('express');
 var router  = express.Router();
 var db = require('../database');
 var TYPES = require('tedious').TYPES;
+var conv_i18n = require('../lib/sql-helper.js').conv_i18n;
+
+
+router.get('/', function(req, res) {
+  db.sql("SELECT * FROM ServiceStation_2lang_utf8")
+		.execute()
+		.then(function (results) {
+			return res.json(results);
+		}).fail(function (err) {
+			return res.status(404).json(err);
+		});
+});
 
 /**
- * @api {get} /api/:lat,:long Request User information
+ * @api {get} /api/stations/nearbyStations/:lat,:long,:lang Request nearby stations
  * @apiName GetNearbyStations
  * @apiGroup Station
  *
- * @apiParam {lat} lat Latitude of the current Location.
- * @apiParam {long} long Longitude of the current Location
+ * @apiParam {String} lat Latitude of the current Location.
+ * @apiParam {String} long Longitude of the current Location
+ * @apiParam {String} lang Language of the interface
  *
  * @apiSuccess {String} Id Unique Id of the station.
- * @apiSuccess {String} AddressLine1 Address of the station.
+ * @apiSuccess {String} Address Address of the station.
  * @apiSuccess {String} City City (province) of the station.
  * @apiSuccess {String} Country Country of the station.
- * @apiSuccess {String} DisplayName Name of the station.
+ * @apiSuccess {String} Name Name of the station.
  * @apiSuccess {String} PostalCode PostalCode of the station.
  * @apiSuccess {Number} Latitude Latitude of the station location.
  * @apiSuccess {Number} Longitude Longitude of the station location.
  */
-router.get('/', function(req, res) {
-  res.json({ message: 'Returns all stations.'});
-});
-
-router.get('/:lat,:long', function (req, res) {
-	db.sql("SELECT TOP(10) Id, AddressLine1, City, Country, DisplayName, PostalCode, Latitude, Longitude\
-	 FROM ServiceStation st\
+router.get('/nearbyStations/:lat,:long,:lang', function (req, res) {
+	db.sql( conv_i18n('Select Id, AddressLine[_lang] Address, City[_lang] City, Country, DisplayName[_lang] Name, PostalCode, Latitude, Longitude',req.params.lang) +
+	 " FROM ServiceStation st\
 	 ORDER BY (geography::Point( @lat, @long, 4326)).STDistance(st.Location)")
 	 	.parameter('lat', TYPES.Numeric, req.params.lat,{precision:'9',scale:'6'})
 		.parameter('long', TYPES.Numeric, req.params.long,{precision:'9',scale:'6'})
+		.execute()
+		.then(function (results) {
+			return res.json(results);
+		}).fail(function (err) {
+			return res.status(404).json(err);
+		});
+});
+
+/**
+ * @api {get} /api/getByKeyword/:keyword,:lang Request stations by keyword
+ * @apiName GetStaionsByKeyword
+ * @apiGroup Station
+ *
+ * @apiParam {String} keyword Keyword used to search for the stations
+ * @apiParam {String} lang Language of the interface
+ *
+ * @apiSuccess {String} Id Unique Id of the station.
+ * @apiSuccess {String} Address Address of the station.
+ * @apiSuccess {String} City City (province) of the station.
+ * @apiSuccess {String} Country Country of the station.
+ * @apiSuccess {String} Name Name of the station.
+ * @apiSuccess {String} PostalCode PostalCode of the station.
+ * @apiSuccess {Number} Latitude Latitude of the station location.
+ * @apiSuccess {Number} Longitude Longitude of the station location.
+ */
+router.get('/getByKeyword/:keyword,:lang', function (req, res) {
+	db.sql(conv_i18n('Select Id, AddressLine[_lang] Address, City[_lang] City, Country, DisplayName[_lang] Name, PostalCode, Latitude, Longitude',req.params.lang) +
+	 " FROM ServiceStation st\
+	 WHERE DisplayName_"+ req.params.lang +" LIKE ('%'+@keyword+'%')\
+	 	OR City_"+ req.params.lang +" LIKE ('%'+@keyword+'%')\
+		 OR AddressLine_"+ req.params.lang +" LIKE ('%'+@keyword+'%')")
+	 	.parameter('keyword', TYPES.NChar, req.params.keyword)
 		.execute()
 		.then(function (results) {
 			return res.json(results);
